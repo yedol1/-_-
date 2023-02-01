7b86a15f6db6dfaacf663738f5264599905983be
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-02-02 02:41:27 +0900

    add: assets 아이콘 사용을 위한 코드 추가

---
+++ src/styles/CommonIcon.jsx
@@ -0,0 +1,11 @@
+import DROPDOWN_ICON from "assets/icons/dropdown_icon.svg";
+import FIRST_PAGE_ON_ICON from "assets/icons/first_page_icon-1.svg";
+import FIRST_PAGE_OFF_ICON from "assets/icons/first_page_icon.svg";
+import LAST_PAGE_ON_ICON from "assets/icons/last_page_icon-1.svg";
+import LAST_PAGE_OFF_ICON from "assets/icons/last_page_icon.svg";
+import NEXT_ON_ICON from "assets/icons/next_icon-1.svg";
+import NEXT_OFF_ICON from "assets/icons/next_icon.svg";
+import PREV_ON_ICON from "assets/icons/prev_icon-1.svg";
+import PREV_OFF_ICON from "assets/icons/prev_icon.svg";
+
+export { DROPDOWN_ICON, FIRST_PAGE_ON_ICON, FIRST_PAGE_OFF_ICON, LAST_PAGE_ON_ICON, LAST_PAGE_OFF_ICON, NEXT_ON_ICON, NEXT_OFF_ICON, PREV_ON_ICON, PREV_OFF_ICON };
