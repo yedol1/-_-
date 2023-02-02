571870c73b426e001fa6a99ef7b348a7783206dc
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-02-02 17:07:00 +0900

    feat: 키워드 옵션 별 검색 기능 추가

--- src/component/SearchBox/SearchBox.jsx
+++ src/component/SearchBox/SearchBox.jsx
@@ -1,11 +1,39 @@
-import React from "react";
-import { DROPDOWN_ICON } from "styles/CommonIcon";
-import style from "component/SearchBox/searchBox.module.css";
+import React, { useEffect, useRef, useState } from 'react';
+import { DROPDOWN_ICON } from 'styles/CommonIcon';
+import style from 'component/SearchBox/searchBox.module.css';
 
 const SearchBox = () => {
+  const categoryList = {
+    all: '전체',
+    title: '상품명',
+    brand: '브랜드',
+    description: '상품내용',
+  };
+  const urlParams = new URLSearchParams(window.location.search);
+  const [currentOptionKey, setCurrentOptionKey] = useState(urlParams.get('option') || 'all');
+  const [keyword, setKeyword] = useState(urlParams.get('keyword') || '');
+  const [limit, setLimit] = useState(urlParams.get('limit') || '10');
+  const [isClicked, setIsClicked] = useState(false);
+  console.log(urlParams.get('limit'));
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    window.history.pushState('', null, `?keyword=${keyword}&option=${currentOptionKey}&limit=${limit}`);
+  }, [currentOptionKey, keyword, limit]);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    inputRef.current.value = urlParams.get('keyword');
+  }, []);
+
+  const onClickHandler = async () => {
+    await setKeyword(inputRef.current.value);
+    window.location.reload();
+  };
   return (
     <header className={style.wrapper}>
-      <h1 className="sr-only">상품 검색창</h1>
+      <h1 className='sr-only'>상품 검색창</h1>
       <div className={style.tit_wrapper}>
         <strong className={style.sub_tit}>상품검색</strong>
       </div>
@@ -13,27 +41,61 @@ const SearchBox = () => {
       <div className={style.search_wrapper}>
         <strong className={style.sub_tit}>검색</strong>
         <div className={style.dropdown_wrapper}>
-          {"전체"}
-          <button className={style.dropdown_btn} type="button">
-            <img src={DROPDOWN_ICON} alt="드롭다운 아이콘" />
+          {categoryList[currentOptionKey]}
+          <button className={style.dropdown_btn} type='button' onClick={() => setIsClicked(!isClicked)}>
+            <img src={DROPDOWN_ICON} alt='드롭다운 아이콘' />
           </button>
-          <ul className={`${style.select_box} sr-only`}>
+          <ul className={isClicked ? `${style.select_box}` : `${style.select_box} sr-only`}>
             <li>
-              <button>전체</button>
+              <button
+                onClick={() => {
+                  setIsClicked(!isClicked);
+                  setCurrentOptionKey('all');
+                }}
+              >
+                전체
+              </button>
             </li>
             <li>
-              <button>상품명</button>
+              <button
+                onClick={() => {
+                  setIsClicked(!isClicked);
+                  setCurrentOptionKey('title');
+                }}
+              >
+                상품명
+              </button>
             </li>
             <li>
-              <button>브랜드</button>
+              <button
+                onClick={() => {
+                  setIsClicked(!isClicked);
+                  setCurrentOptionKey('brand');
+                }}
+              >
+                브랜드
+              </button>
             </li>
             <li>
-              <button>상품내용</button>
+              <button
+                onClick={() => {
+                  setIsClicked(!isClicked);
+                  setCurrentOptionKey('description');
+                }}
+              >
+                상품내용
+              </button>
             </li>
           </ul>
         </div>
-        <input type="text" />
-        <button className={style.search_button} type="submit">
+        <input ref={inputRef} type='text' />
+        <button
+          className={style.search_button}
+          onClick={() => {
+            onClickHandler();
+          }}
+          type='submit'
+        >
           조회
         </button>
       </div>
