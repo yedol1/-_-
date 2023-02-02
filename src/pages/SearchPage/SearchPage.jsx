c4496c9a7585374a59f484cf1eb486bc7262c271
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-02-02 17:08:07 +0900

    feat: 데이터 요청 기능 추가

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,17 +1,20 @@
-import SearchBox from "component/SearchBox/SearchBox";
-import SearchLists from "component/SearchLists/SearchLists";
-import React from "react";
-import styles from "pages/SearchPage/searchPage.module.css";
+import SearchBox from 'component/SearchBox/SearchBox';
+import SearchLists from 'component/SearchLists/SearchLists';
+import React, { useState } from 'react';
+import styles from 'pages/SearchPage/searchPage.module.css';
+import { useQuery } from 'react-query';
+
+const getData = async () => {
+  return await fetch(process.env.REACT_APP_API_ROOT).then((response) => response.json());
+};
 const SearchPage = () => {
+  const { isLoading, data } = useQuery(['product_list'], getData);
   return (
     <>
       <SearchBox />
       <main className={styles.main_wrapper}>
-        <h2 className="sr-only">상품리스트</h2>
-        <div className={styles.tit_wrapper}>
-          <p>{`검색된 데이터 : ${"100"}건`}</p>
-        </div>
-        <SearchLists />
+        <h2 className='sr-only'>상품리스트</h2>
+        <SearchLists data={data} />
       </main>
     </>
   );
