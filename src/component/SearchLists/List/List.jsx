203e1c32ce3c019eb2da9c63bd313d3b7b3bc07f
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-02-02 17:12:22 +0900

    feat: 프롭스를 전달받아 각 위치에 맞게 할당

--- src/component/SearchLists/List/List.jsx
+++ src/component/SearchLists/List/List.jsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React from 'react';
 
-import styles from "component/SearchLists/List/list.module.css";
+import styles from 'component/SearchLists/List/list.module.css';
 
-const List = () => {
-  const description = "An apple mobile which is nothing like apple";
+const List = ({ id, title, brand, description, price, rating, stock }) => {
   return (
     <>
       <tr className={styles.tr_style} key={1}>
-        <td>{"1"}</td>
-        <td>{"iPhone 9"}</td>
-        <td>{"Apple"}</td>
-        <td>{description.length <= 40 ? description : description.slice(0, 40) + "..."}</td>
-        <td>{`$ ${549}`}</td>
-        <td>{4.69}</td>
-        <td>{94}</td>
+        <td>{id}</td>
+        <td>{title}</td>
+        <td>{brand}</td>
+        <td>{description.length <= 40 ? description : description.slice(0, 40) + '...'}</td>
+        <td>{`$ ${price}`}</td>
+        <td>{rating}</td>
+        <td>{stock}</td>
       </tr>
     </>
   );
